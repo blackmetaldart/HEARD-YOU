6fce4c61809312f7d4a16b116463251ee932277f
Alexander <mrbeachcode@gmail.com>
2019-11-03 15:04:46 -0500

    added the song controller methods

--- music-api/src/main/java/com/example/musicapi/controller/SongController.java
+++ music-api/src/main/java/com/example/musicapi/controller/SongController.java
@@ -1,4 +1,36 @@
 package com.example.musicapi.controller;
 
+import com.example.musicapi.model.Song;
+import com.example.musicapi.service.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/song")
 public class SongController {
+
+    @Autowired
+    SongService songService;
+
+    //THIS ENDPOINT ALLOWS A USER WITH THE ARTIST ROLE TO CREATE A SONG
+    @PostMapping
+    public Song createSong(@RequestBody Song song){
+        return songService.createSong(song);
+    }
+
+    //THIS ENDPOINT ALLOWS A USER TO ACCESS THE SONG LIST
+    @GetMapping("/list")
+    public List<Song> listSongs(){
+        return songService.listSongs();
+    }
+
+    //THIS ENDPOINT ALLOWS THE USER WITH THE ROLE ADMIN TO DELETE A SONG BY ITS ID
+    @DeleteMapping("/{songId}")
+    public void deleteSongById(@PathVariable Long songId) {
+        songService.deleteSongById(songId);
+    }
+
+
 }
