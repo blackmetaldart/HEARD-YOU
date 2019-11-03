0c23516048c407c39c4e23630e9dea2eb4921bfc
Alexander <mrbeachcode@gmail.com>
2019-11-03 15:20:30 -0500

    Added some security functionality

--- users-api/src/main/java/com/example/usersapi/service/UserServiceImpl.java
+++ users-api/src/main/java/com/example/usersapi/service/UserServiceImpl.java
@@ -4,7 +4,14 @@ import com.example.usersapi.model.User;
 import com.example.usersapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -44,4 +51,55 @@ public class UserServiceImpl implements UserService{
         userRepository.save(user);
         return HttpStatus.OK;
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = getUser(username);
+
+        if(user==null)
+            throw new UsernameNotFoundException("User null");
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), bCryptPasswordEncoder.encode(user.getPassword()),
+                true, true, true, true, getGrantedAuthorities(user));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(User user){
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(user.getUserRole().getName()));
+
+        return authorities;
+    }
+
+    @Override
+    public String createUser(User newUser) {
+        UserRole userRole = userRoleService.getRole(newUser.getUserRole().getName());
+        newUser.setUserRole(userRole);
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        if(userRepository.save(newUser) != null){
+            UserDetails userDetails = loadUserByUsername(newUser.getUsername());
+            return jwtUtil.generateToken(userDetails);
+        }
+        return null;
+    }
+
+    //THIS METHOD GENERATES A TOKEN IF THE USERNAME AND PASSWORD MATCH THAT OF A USER IN THE USER REPOSITORY
+    @Override
+    public String login(User user){
+        User newUser = userRepository.findByUsername(user.getUsername());
+        //userRepository.login(user.getUsername(), user.getPassword()) != null
+        if(newUser != null && bCryptPasswordEncoder.matches(user.getPassword(), newUser.getPassword())){
+            UserDetails userDetails = loadUserByUsername(newUser.getUsername());
+            return jwtUtil.generateToken(userDetails);
+        }
+        return null;
+    }
+
+    //THIS DELETES A USER BY THE USER ID
+    @Override
+    public HttpStatus deleteById(Long userId){
+        userRepository.deleteById(userId);
+        return HttpStatus.OK;
+    }
+
 }
