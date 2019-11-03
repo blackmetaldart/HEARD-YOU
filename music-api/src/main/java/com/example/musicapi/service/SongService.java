a9037049f1cda1d027de853a50e502b47c0a9ebe
Alexander <mrbeachcode@gmail.com>
2019-11-03 15:00:57 -0500

    the model for a song

--- music-api/src/main/java/com/example/musicapi/service/SongService.java
+++ music-api/src/main/java/com/example/musicapi/service/SongService.java
@@ -1,4 +1,14 @@
 package com.example.musicapi.service;
 
+import com.example.musicapi.model.Song;
+
+import java.util.List;
+
 public interface SongService {
+
+    public Song createSong(Song song);
+
+    public List<Song> listSongs();
+
+    public void deleteSongById(Long songId);
 }
