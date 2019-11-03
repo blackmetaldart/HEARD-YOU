c48e36e03166ee67f26c7729593894074d0a5be6
Alexander <mrbeachcode@gmail.com>
2019-11-03 15:03:22 -0500

    implemented the song service interface

--- music-api/src/main/java/com/example/musicapi/service/SongServiceImpl.java
+++ music-api/src/main/java/com/example/musicapi/service/SongServiceImpl.java
@@ -1,4 +1,43 @@
 package com.example.musicapi.service;
 
-public class SongServiceImpl {
+import com.example.musicapi.model.Song;
+import com.example.musicapi.repository.SongRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SongServiceImpl implements SongService {
+
+    @Autowired
+    SongRepository songRepository;
+
+    @Autowired
+    CSVReader csvReader;
+
+    /**
+     * The song repository extends the CrudRepository which has a save method
+     * @param song
+     * @return
+     */
+    @Override
+    public Song createSong(Song song) {
+        return songRepository.save(song);
+    }
+
+    /**
+     * The SongRepository extends the CrudRepository which has a findAll method
+     * @return
+     */
+    @Override
+    public List<Song> listSongs(){
+        return csvReader.readSongsFromCSV("/Users/marksman/Desktop/OwnWork/GADA-Work/GADA-PROJECTS/Swap-app/songs.csv");
+    }
+
+    @Override
+    public void deleteSongById(Long songId){
+        songRepository.deleteById(songId);
+    }
 }
