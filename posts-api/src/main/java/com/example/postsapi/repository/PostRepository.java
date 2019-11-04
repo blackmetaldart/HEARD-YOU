57d2d4dc715b297af9db8f23143c66754c6691fc
Alexander <mrbeachcode@gmail.com>
2019-11-04 04:03:27 -0500

    created the post repository

---
+++ posts-api/src/main/java/com/example/postsapi/repository/PostRepository.java
@@ -0,0 +1,16 @@
+package com.example.postsapi.repository;
+
+import com.example.postsapi.model.Post;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+    @Query("FROM Post post WHERE post.username = ?1")
+    List<Post> findAllByUsername(String username);
+
+    @Query("FROM Post post WHERE post.id = ?1")
+    Post getPostById (Long postId);
+
+}
