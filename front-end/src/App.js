582800e793eb3d2282a825eda44f71924cc852e1
Alexander <mrbeachcode@gmail.com>
2019-11-04 17:18:41 -0500

    found a typo in the react app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Header from './Header';
 import Nav from './Nav';
-import Main from './Main'
+import Main from './Main';
 import Footer from './Footer';
 
 import './App.css';
