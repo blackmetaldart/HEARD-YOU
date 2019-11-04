a64226fc6b44b7367c008238afce81e8e9862520
Alexander <mrbeachcode@gmail.com>
2019-11-04 17:45:53 -0500

    created the nav component for the react front page

---
+++ front-end/src/Nav.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+  render(){
+      return (
+        <div className="Nav">
+
+  <h1>This is nav 1 </h1>
+  <h1>THis is nav 2 </h1>
+  <h1>This is nav 3 </h1>
+        </div>
+      );
+    }
+}
+
+export default App;
