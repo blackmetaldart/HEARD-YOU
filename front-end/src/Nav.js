54de56d37b0ea9f7f2ba2e57a07cdac1d7b221a9
Alexander <mrbeachcode@gmail.com>
2019-11-04 17:48:03 -0500

    made the main page for the react front end

--- front-end/src/Nav.js
+++ front-end/src/Nav.js
@@ -4,13 +4,12 @@ class Nav extends Component {
   render(){
       return (
         <div className="Nav">
-
-  <h1>This is nav 1 </h1>
-  <h1>THis is nav 2 </h1>
-  <h1>This is nav 3 </h1>
+          <h1>This is nav 1 </h1>
+          <h1>THis is nav 2 </h1>
+          <h1>This is nav 3 </h1>
         </div>
       );
     }
 }
 
-export default App;
+export default Nav;
