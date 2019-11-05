1ebca0e1a3d8c2c6be8cf5696bcc831b23ae9bf8
Alexander <mrbeachcode@gmail.com>
2019-11-05 12:06:31 -0500

    Removed microserves that weren't necessary

--- music-api/src/main/java/com/example/musicapi/repository/SongRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.musicapi.repository;
-
-import com.example.musicapi.model.Song;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SongRepository extends CrudRepository<Song, Long> {
-
-}
