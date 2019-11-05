bb6527bb0cf0129f171be0c82b63759d548e8af9
Alexander <mrbeachcode@gmail.com>
2019-11-05 18:18:07 -0500

    added the user service and its implementation

---
+++ users-api/src/main/java/com/example/usersapi/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.usersapi.service;
+
+public interface UserService {
+    String createUser();
+    String login();
+    String createProfile();
+    String updateProfile();
+    String logout();
+}
---
+++ users-api/src/main/java/com/example/usersapi/service/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.usersapi.service;
+
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public String createUser() {
+        return null;
+    }
+
+    @Override
+    public String login() {
+        return null;
+    }
+
+    @Override
+    public String createProfile() {
+        return null;
+    }
+
+    @Override
+    public String updateProfile() {
+        return null;
+    }
+
+    @Override
+    public String logout() {
+        return null;
+    }
+}
