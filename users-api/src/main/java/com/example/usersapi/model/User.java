311da1ddce9a68d38d5194ef224bb8972086e365
Alexander <mrbeachcode@gmail.com>
2019-11-06 10:46:40 -0500

    fixed an error in the user model

--- users-api/src/main/java/com/example/usersapi/model/User.java
+++ users-api/src/main/java/com/example/usersapi/model/User.java
@@ -49,7 +49,7 @@ public class User {
     public String getUsername() {return username;}
     public void setUsername(String username) {this.username = username;}
 
-    public String password() {return password;}
+    public String getPassword() {return password;}
     public void setPassword(String password) {this.password = password;}
 
 }
