0a4fed5756a8bec1da85c60a2d43b4c37517fa0e
Alexander <mrbeachcode@gmail.com>
2019-11-06 10:45:07 -0500

    added two queries to the repository for user api service

--- users-api/src/main/java/com/example/usersapi/repository/UserRepository.java
+++ users-api/src/main/java/com/example/usersapi/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.example.usersapi.repository;
 
 import com.example.usersapi.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername (String username);
+
+    @Query("FROM User u WHERE u.username = ?1 and u.password = ?2")
+    User login(String username, String password);
+
 }
