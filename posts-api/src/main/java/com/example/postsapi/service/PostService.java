d8388946cf368861dd84d4c67bb11319c7b027c7
Alexander <mrbeachcode@gmail.com>
2019-11-06 11:18:51 -0500

    added parameters to the post service

--- posts-api/src/main/java/com/example/postsapi/service/PostService.java
+++ posts-api/src/main/java/com/example/postsapi/service/PostService.java
@@ -1,7 +1,9 @@
 package com.example.postsapi.service;
 
+import com.example.postsapi.model.Post;
+
 public interface PostService {
-    String createPost();
+    String createPost(String username, Post post);
     String findPostByPostId();
     String findPostBySongId();
     String deletePost();
