1e78b1eafffc13ed16e894eaa584651dac1e874c
Alexander <mrbeachcode@gmail.com>
2019-11-06 10:33:56 -0500

    added a parameter to the create user method

--- users-api/src/main/java/com/example/usersapi/service/UserService.java
+++ users-api/src/main/java/com/example/usersapi/service/UserService.java
@@ -1,7 +1,9 @@
 package com.example.usersapi.service;
 
+import com.example.usersapi.model.User;
+
 public interface UserService {
-    String createUser();
+    String createUser(User user);
     String login();
     String createProfile();
     String updateProfile();
