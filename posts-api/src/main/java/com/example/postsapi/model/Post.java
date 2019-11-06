46dbe28ee527eccf82c22727c6411f71f8dfc4cb
Alexander <mrbeachcode@gmail.com>
2019-11-05 19:46:17 -0500

    fleshed out the post model for the api

--- posts-api/src/main/java/com/example/postsapi/model/Post.java
+++ posts-api/src/main/java/com/example/postsapi/model/Post.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name="post")
 public class Post {
 
-    // ID / TITLE / DESCRIPTION / USERNAME COLUMNS FOR TABLE
+    // ID / TITLE / DESCRIPTION / USERNAME / SONGID COLUMNS FOR TABLE
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,5 +21,23 @@ public class Post {
     @Column
     private String username;
 
+    @Column
+    private Long songId;
+
+    //EMPTY CONSTRUCTOR FOR THE POSTS
     public Post(){}
+
+    //GETTERS AND SETTERS FOR THE POST DATA
+    public String getTitle(){return title;}
+    public void setTitle(String title){this.title = title;}
+
+    public String getDescription(){return description;}
+    public void setDescription(String description){this.description = description;}
+
+    public String getUsername(){return username;}
+    public void setUsername(String username){this.username = username;}
+
+    public Long getSongId(){return songId;}
+    public void setSongId(Long songId){this.songId = songId;}
+
 }
