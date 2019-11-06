0c5745746ed07687d193b2a72ad1e9db60739ba0
Alexander <mrbeachcode@gmail.com>
2019-11-06 13:15:32 -0500

    fixed the sql chart for songs. it now inserts the song data

--- songs-api/src/main/resources/db.migration/V1_3__Songs.sql
+++ songs-api/src/main/resources/db.migration/V1_3__Songs.sql
@@ -1,3 +1,26 @@
-CREATE TABLE test_data (
- value VARCHAR(25) NOT NULL PRIMARY KEY
+CREATE TABLE Songs (
+ id SERIAL UNIQUE,
+ title VARCHAR(50),
+ songLength INTEGER,
+ artist VARCHAR(50)
 );
+
+INSERT INTO Songs (name) VALUES ('Bad Guy',3,'Billie Eilish');
+INSERT INTO Songs (name) VALUES ('Old Town Road',2,'Lil Nas X');
+INSERT INTO Songs (name) VALUES ('Sucker',6,'Jonas Brothers');
+INSERT INTO Songs (name) VALUES ('How Do You Sleep?',4,'Sam Smith');
+INSERT INTO Songs (name) VALUES ('Panini',3,'Lil Nas X');
+INSERT INTO Songs (name) VALUES ('Truth Hurts',6,'Lizzo');
+INSERT INTO Songs (name) VALUES ('Motivation',3,'Normani');
+INSERT INTO Songs (name) VALUES ('Slide Away',5,'Miley Cyrus');
+INSERT INTO Songs (name) VALUES ('break up with your girlfriend',4,'Ariana Grande');
+INSERT INTO Songs (name) VALUES ('Talk',7,'Khalid');
+INSERT INTO Songs (name) VALUES ('I Don’t Care',4,'Justin Bieber');
+INSERT INTO Songs (name) VALUES ('Liar',8,'Camila Cabello');
+INSERT INTO Songs (name) VALUES ('No Guidance',4,'Chris Brown');
+INSERT INTO Songs (name) VALUES ('One Thing Right',5,'Kane Brown');
+INSERT INTO Songs (name) VALUES ('Hot Girl Summer',3,'Megan Thee Stallion');
+INSERT INTO Songs (name) VALUES ('Don’t Call Me Up',5,'Mabel');
+INSERT INTO Songs (name) VALUES ('See You Again',7,'Mark Ronson');
+INSERT INTO Songs (name) VALUES ('Harder',2,'Jax Jones');
+INSERT INTO Songs (name) VALUES ('Good Thing',7,'Zedd');
