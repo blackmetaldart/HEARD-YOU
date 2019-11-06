ee4c5c194f0e9defc23e3be9c49f73c72b013ff5
Alexander <mrbeachcode@gmail.com>
2019-11-05 20:06:03 -0500

    finished the model for the songs. fixed the error in the song repository

--- songs-api/src/main/java/com/example/songsapi/model/Song.java
+++ songs-api/src/main/java/com/example/songsapi/model/Song.java
@@ -1,5 +1,15 @@
 package com.example.songsapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
+@Entity
+@Table(name = "song")
 public class Song {
     //ID / TITLE / SONG-LENGTH / ARTIST COLUMNS FOR TABLE
     @Id
@@ -15,4 +25,40 @@ public class Song {
 
     @Column
     private String artist;
+
+    //CONSTRUCTORS FOR THE SONG MODELS
+    //EMPTY SONG CONSTRUCTOR
+    public Song() {}
+
+    //CONSTRUCTOR FOR THE READ SONGS
+    public Song(String title, int songLength, String artist) {
+        this.title = title;
+        this.songLength = songLength;
+        this.artist = artist;
+    }
+
+    //GETTERS AND SETTERS FOR THE COLUMNS
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getSongLength() {
+        return songLength;
+    }
+
+    public void setSongLength(int songLength) {
+        this.songLength = songLength;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
 }
