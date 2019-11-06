eecc3143a22fa1a557667e79dce8c4e4802d7eca
Alexander <mrbeachcode@gmail.com>
2019-11-06 11:21:41 -0500

    added a method to the post service

--- posts-api/src/main/java/com/example/postsapi/service/PostService.java
+++ posts-api/src/main/java/com/example/postsapi/service/PostService.java
@@ -2,8 +2,11 @@ package com.example.postsapi.service;
 
 import com.example.postsapi.model.Post;
 
+import java.util.List;
+
 public interface PostService {
     String createPost(String username, Post post);
+    List<Post> getPostsByUsername(String username);
     String findPostByPostId();
     String findPostBySongId();
     String deletePost();
