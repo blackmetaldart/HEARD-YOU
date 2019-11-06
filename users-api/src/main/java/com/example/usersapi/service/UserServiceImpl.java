102c0e81bc6223c885a0c6a293fc1e7e2314ae3d
Alexander <mrbeachcode@gmail.com>
2019-11-06 10:33:09 -0500

    added the create user method

--- users-api/src/main/java/com/example/usersapi/service/UserServiceImpl.java
+++ users-api/src/main/java/com/example/usersapi/service/UserServiceImpl.java
@@ -1,9 +1,17 @@
 package com.example.usersapi.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public class UserServiceImpl implements UserService{
 
     @Override
-    public String createUser() {
+    public String createUser(User newUser) {
+
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        if(userRepository.save(newUser) != null){
+            UserDetails userDetails = loadUserByUsername(newUser.getUsername());
+            return jwtUtil.generateToken(userDetails);
+        }
         return null;
     }
 
