7e460e94a4e3824d5c61c89936bade135bb9c8f1
Alexander <mrbeachcode@gmail.com>
2019-11-05 19:17:35 -0500

    created the post service

---
+++ posts-api/src/main/java/com/example/postsapi/service/PostServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.postsapi.service;
+
+public class PostServiceImpl implements PostService {
+    @Override
+    public String createPost() {
+        return null;
+    }
+
+    @Override
+    public String findPost() {
+        return null;
+    }
+
+    @Override
+    public String updatePOst() {
+        return null;
+    }
+
+    @Override
+    public String deletePost() {
+        return null;
+    }
+}
