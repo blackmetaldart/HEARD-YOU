19bbaf121e5019c56b919aaa55f5bf5a63347e5f
Alexander <mrbeachcode@gmail.com>
2019-11-06 14:48:32 -0500

    forgot the Service annotation

--- users-api/src/main/java/com/example/usersapi/service/UserServiceImpl.java
+++ users-api/src/main/java/com/example/usersapi/service/UserServiceImpl.java
@@ -10,10 +10,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
