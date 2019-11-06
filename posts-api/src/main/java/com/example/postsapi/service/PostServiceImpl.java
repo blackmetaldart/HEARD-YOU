736b9016a5584ae04e7f6d21548b60e6efeabd96
Alexander <mrbeachcode@gmail.com>
2019-11-06 11:17:17 -0500

    defined the create post method

--- posts-api/src/main/java/com/example/postsapi/service/PostServiceImpl.java
+++ posts-api/src/main/java/com/example/postsapi/service/PostServiceImpl.java
@@ -1,18 +1,24 @@
 package com.example.postsapi.service;
 
+import com.example.postsapi.model.Post;
+import com.example.postsapi.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PostServiceImpl implements PostService {
+
+    @Autowired
+    PostRepository postRepository;
+
     @Override
-    public String createPost() {
-        return null;
+    public Post createPost(String username, Post post) {
+        post.setUsername(username);
+        return postRepository.save(post);
     }
 
     @Override
     public String findPost() {
-        return null;
-    }
-
-    @Override
-    public String updatePOst() {
         return null;
     }
 
