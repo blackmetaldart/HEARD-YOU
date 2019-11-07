b55a1e793f7b821d9059eabbe8bb75c62c4da743
Alexander <mrbeachcode@gmail.com>
2019-11-07 13:33:38 -0500

    added an annotation to the authentication

--- users-api/src/main/java/com/example/usersapi/config/AuthenticationFacadeImpl.java
+++ users-api/src/main/java/com/example/usersapi/config/AuthenticationFacadeImpl.java
@@ -2,7 +2,9 @@ package com.example.usersapi.config;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthenticationFacadeImpl implements AuthenticationFacade{
 
     @Override
