494be69705e3879541c0aa2480feede3867162b9
Alexander <mrbeachcode@gmail.com>
2019-11-07 13:10:31 -0500

    made a backup command in case the script doesn't create the table

--- users-api/src/main/java/com/example/usersapi/service/UserServiceImpl.java
+++ users-api/src/main/java/com/example/usersapi/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.usersapi.service;
 import com.example.usersapi.config.JwtUtil;
 import com.example.usersapi.model.User;
 import com.example.usersapi.repository.UserRepository;
+import com.example.usersapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,12 +12,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     @Autowired
     JwtUtil jwtUtil;
@@ -28,10 +29,9 @@ public class UserServiceImpl implements UserService{
     @Qualifier("encoder")
     PasswordEncoder bCryptPasswordEncoder;
 
-
+    //CREATES A USER AND GENERATES A JWT TOKEN FOR AUTHENTICATION
     @Override
     public String createUser(User newUser) {
-
         newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
         if(userRepository.save(newUser) != null){
             UserDetails userDetails = loadUserByUsername(newUser.getUsername());
@@ -40,11 +40,8 @@ public class UserServiceImpl implements UserService{
         return null;
     }
 
-    @Override
-    public User getUser(String username) {
-        return userRepository.findByUsername(username);
-    }
-
+    //COMPARES THE USER PASSED AS A PARAMETER TO THE INFORMATION IN THE USER REPOSITORY AND
+    // GENERATES A JWT TOKEN ON APPROVAL
     @Override
     public String login(User user){
         User newUser = userRepository.findByUsername(user.getUsername());
@@ -55,28 +52,30 @@ public class UserServiceImpl implements UserService{
         return null;
     }
 
+    //FINDS A USER BY THE USERNAME
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = getUser(username);
+        if(user==null)
+            throw new UsernameNotFoundException("User null");
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), bCryptPasswordEncoder.encode(user.getPassword()),
+                true, true, true, true, new ArrayList<>());
+    }
+
+    //FINDS A PROFILE BY THE USERNAME FROM THE USER REPOSITORY
+    @Override
+    public User getUser(String username) {
+        return userRepository.findByUsername(username);
+    }
 
     @Override
     public String logout() {
         return null;
     }
 
-    //FINDS A USER BY THE USERNAME
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = getUser(username);
-
-        if(user==null)
-            throw new UsernameNotFoundException("User null");
-
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), bCryptPasswordEncoder.encode(user.getPassword()),
-                true, true, true, true, new ArrayList<>());
+    //    GRANTS A USER AUTHORITY
+    private List<GrantedAuthority> getGrantedAuthorities(User user){
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+//        authorities.add(new SimpleGrantedAuthority(user.getUserRole().getName()));
+        return authorities;
     }
-
-    //GRANTS A USER AUTHORITY
-//    private List<GrantedAuthority> getGrantedAuthorities(User user){
-//        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-//        authorities.add(new SimpleGrantedAuthority(user.getUsername()));
-//        return authorities;
-//    }
-
 }
