431192bcebc66f91c002734305e0035601394009
Alexander <mrbeachcode@gmail.com>
2019-11-07 10:31:17 -0500

    made a backup command in case the script doesn't create the table

--- users-api/createDatabaseAndUser.sh
+++ users-api/createDatabaseAndUser.sh
@@ -3,3 +3,4 @@ set -e
 
 psql -U postgres -c "CREATE ROLE postgres LOGIN  PASSWORD 'password'"
 psql -U postgres -d postgres -c "CREATE DATABASE runnerdev OWNER postgres"
+psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE runnerdev TO postgres"
