af6c2844f674892730b0584414ca22e2f7df545d
Alexander <mrbeachcode@gmail.com>
2019-11-08 13:21:33 -0500

    changed the username back to postgres

--- users-api/createDatabaseAndUser.sh
+++ users-api/createDatabaseAndUser.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-psql -U postgres -c "CREATE ROLE marksman LOGIN  PASSWORD 'password'"
-psql -U postgres -d postgres -c "CREATE DATABASE runnerdev OWNER marksman"
-psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE runnerdev TO marksman"
+psql -U postgres -c "CREATE ROLE postgres LOGIN  PASSWORD 'password'"
+psql -U postgres -d postgres -c "CREATE DATABASE runnerdev OWNER postgres"
+psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE runnerdev TO postgres"
