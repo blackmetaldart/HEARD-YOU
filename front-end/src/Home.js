972a45779f51b9381699dc25fdf76749b61832b6
Alexander <mrbeachcode@gmail.com>
2019-11-10 13:37:05 -0500

    imported the Login Screen to the home page

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,22 +1,12 @@
 import React, {Component} from 'react';
+import LoginScreen from './LoginScreen';
 import './Home.css';
 
 class Home extends Component {
   render(){
       return (
         <div className="Home">
-          <h2> Home Page </h2>
-
-          <h3> Sign Up <h3>
-          <p> ENTER USERNAME TEXT BOX HERE <p>
-          <p> ENTER PASSWORD TEXT BOX HERE <p>
-          <p> BUTTON FOR SUBMIT <p>
-
-          <h3> Log In <h3>
-          <p> ENTER USERNAME TEXT BOX HERE <p>
-          <p> ENTER PASSWORD TEXT BOX HERE <p>
-          <p> BUTTON FOR SUBMIT <p>
-        </div>
+          <LoginScreen />
       );
     }
 }
