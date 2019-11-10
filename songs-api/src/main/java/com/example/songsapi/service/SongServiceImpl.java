71b61095bc96dbdec23fbf0f02b00690029a5881
Alexander <mrbeachcode@gmail.com>
2019-11-10 15:29:31 -0500

    added the service annotation to the songs service impl

--- songs-api/src/main/java/com/example/songsapi/service/SongServiceImpl.java
+++ songs-api/src/main/java/com/example/songsapi/service/SongServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.songsapi.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SongServiceImpl implements SongService {
     @Override
     public String listSongs() {
