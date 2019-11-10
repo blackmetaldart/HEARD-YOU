fc16278284afd7d416a282d07f2a5f2110b4745a
Alexander <mrbeachcode@gmail.com>
2019-11-10 14:02:05 -0500

    made the template for the register component

---
+++ front-end/src/Register.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Register extends Component {
+  render(){
+      return (
+        <div className="register">
+          <h3> Register Here <h3>
+          <p> ENTER USERNAME TEXT BOX HERE <p>
+          <p> ENTER PASSWORD TEXT BOX HERE <p>
+          <p> BUTTON FOR SUBMIT <p>
+        </div>
+      );
+    }
+}
+
+export default Register;
