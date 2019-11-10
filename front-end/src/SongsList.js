f10ea904f041803e00ca0747ef6ce2288c679d7d
Alexander <mrbeachcode@gmail.com>
2019-11-10 15:28:42 -0500

    made the fetch call for the songs list

--- front-end/src/SongsList.js
+++ front-end/src/SongsList.js
@@ -4,9 +4,29 @@ class SongsList extends Component {
   constructor(props) {
   super(props);
 
-  this.state = 
+  this.state = {
+    localApiLoaded : false,
+    localApiUrl: '',
+    songs : [],
+    localApiError : true
+  }
 }
 
+componentDidMount{
+  fetch('http://localhost:8082/songs/list')
+  .then(response => {
+      return response.json();
+    }).then(response => {
+      console.log(response);
+      this.setState({
+        songs : response
+      });
+    }).catch(error => {
+      this.setState({
+        localApiError : false
+      })
+    })
+}
   render(){
       return (
         <div className="SongsList">
