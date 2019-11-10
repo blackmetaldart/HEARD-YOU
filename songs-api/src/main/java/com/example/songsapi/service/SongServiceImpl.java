93cc218dcf9970b4158cca808950433ed873cb0c
Alexander <mrbeachcode@gmail.com>
2019-11-10 15:32:35 -0500

    autowired song service and put the list songs function behind the list songs endpoint

--- songs-api/src/main/java/com/example/songsapi/service/SongServiceImpl.java
+++ songs-api/src/main/java/com/example/songsapi/service/SongServiceImpl.java
@@ -1,11 +1,17 @@
 package com.example.songsapi.service;
 
+import com.example.songsapi.model.Song;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SongServiceImpl implements SongService {
+    @Autowired
+    SongService songService;
+
     @Override
-    public String listSongs() {
-        return null;
+    public Iterable<Song> listSongs() {
+        return songService.listSongs();
     }
+
 }
