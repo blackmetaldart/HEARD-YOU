47fe8be2384aa2e05e098ae548fd96adeb5f0135
Alexander <mrbeachcode@gmail.com>
2019-11-09 19:08:55 -0500

    added repository to try and get rid of the bean error

--- users-api/src/main/java/com/example/usersapi/repository/UserRepository.java
+++ users-api/src/main/java/com/example/usersapi/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.usersapi.repository;
 import com.example.usersapi.model.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername (String username);
 
