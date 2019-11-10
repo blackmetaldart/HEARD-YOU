f1da0605b9c1c8e7a282e60b08fc9c8e30f35514
Alexander <mrbeachcode@gmail.com>
2019-11-10 16:51:14 -0500

    imported the post component into the posts page. preparing to create the post component

--- front-end/src/Posts.js
+++ front-end/src/Posts.js
@@ -1,12 +1,49 @@
 import React, {Component} from 'react';
 import './Posts';
+import Post from './components/Post';
+
 
 class Posts extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    localApiLoaded : false,
+    localApiUrl: '',
+    postsData : [],
+    localApiError : true
+  }
+}
+
+componentDidMount() {
+  fetch('http://localhost:8083/list')
+  .then(response => {return response.json();})
+  .then(response => {
+      console.log(response);
+      this.setState({
+        postsData : response
+      });
+    }).catch(error => {
+      this.setState({
+        localApiError : false
+      })
+    })
+  }
+
   render(){
       return (
         <div className="Posts">
-          <h2> The Top Posts For The Week </h2>
-
+          <h2> The Posts For The Week </h2>
+          <div className="postListDiv">
+          {this.state.postData ? (this.state.postData.map((postInfo, key) => {
+          return (
+          <div className="postDiv">
+            <Post key={key} postProp={postInfo}/>
+          </div>
+          )})
+        ) : ''
+        }
+          </div>
         </div>
       );
     }
