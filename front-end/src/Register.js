5f508b0b0093fe0db3b37b813be22e876b800cde
Alexander <mrbeachcode@gmail.com>
2019-11-10 14:20:45 -0500

    added a label to the register form

--- front-end/src/Register.js
+++ front-end/src/Register.js
@@ -4,6 +4,7 @@ class Register extends Component {
   render(){
       return (
         <div className="register">
+          <h3> Register Here <h3>
             <form>
               <label>
                 USERNAME:
