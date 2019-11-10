3826a1d96e7641f0350ce404be50a649eb9e7c12
Alexander <mrbeachcode@gmail.com>
2019-11-10 13:35:07 -0500

    transferred the template into the LogIn Screen

---
+++ front-end/src/LoginScreen.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class LoginScreen extends Component {
+  render(){
+      return (
+        <div className="LoginScreen">
+          <h2> Home Page </h2>
+
+          <h3> Register Here <h3>
+          <p> ENTER USERNAME TEXT BOX HERE <p>
+          <p> ENTER PASSWORD TEXT BOX HERE <p>
+          <p> BUTTON FOR SUBMIT <p>
+
+          <h3> Log In Here <h3>
+          <p> ENTER USERNAME TEXT BOX HERE <p>
+          <p> ENTER PASSWORD TEXT BOX HERE <p>
+          <p> BUTTON FOR SUBMIT <p>
+        </div>
+      );
+    }
+}
+
+export default LoginScreen;
