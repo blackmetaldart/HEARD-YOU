9aa36ba225de13277f607ecc025c97bf34a63a46
Alexander <mrbeachcode@gmail.com>
2019-11-10 13:40:07 -0500

    added the router to switch between log in and register

--- front-end/src/LoginScreen.js
+++ front-end/src/LoginScreen.js
@@ -1,5 +1,13 @@
 import React, {Component} from 'react';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+
 class LoginScreen extends Component {
   render(){
       return (
