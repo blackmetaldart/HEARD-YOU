50b9ffbed920e80484e8d7211ecbc93afc7cab87
Alexander <mrbeachcode@gmail.com>
2019-11-10 16:32:05 -0500

    added the user profile div. preparing to create the component

--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -1,12 +1,47 @@
 import React, {Component} from 'react';
 import './Profile.css';
+import UserProfile from './components/UserProfile';
 
 class Profile extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    localApiLoaded : false,
+    localApiUrl: '',
+    userData : [],
+    localApiError : true
+  }
+}
+
+componentDidMount() {
+  fetch('http://localhost:8081/{username}/profile')
+  .then(response => {return response.json();})
+  .then(response => {
+      console.log(response);
+      this.setState({
+        userData : response
+      });
+    }).catch(error => {
+      this.setState({
+        localApiError : false
+      })
+    })
+  }
   render(){
       return (
         <div className="Profile">
-          <h2> Profile Page </h2>
-
+            <h2> Account Profile </h2>
+            <div className="userProfile">
+            {this.state.userData ? (this.state.userData.map((userInfo, key) => {
+            return (
+            <div className="userProfileDiv">
+              <UserProfile key={key} userDataProp={userInfo}/>
+            </div>
+            )})
+          ) : ''
+          }
+            </div>
         </div>
       );
     }
