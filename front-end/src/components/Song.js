f959687a3e3f319d25c29cef43fec1f99cd132a4
Alexander <mrbeachcode@gmail.com>
2019-11-10 17:03:23 -0500

    added a make post button to the songs

--- front-end/src/components/Song.js
+++ front-end/src/components/Song.js
@@ -6,6 +6,7 @@ class Song extends Component {
       <div>
         <h3>Song : {this.props.songProp.title} [{this.props.songProp.songLength} mins]</h3>
         <h4>{this.props.songProp.artist[0]}</h4>
+        <input type="submit" value="MAKE POST" />
       </div>
       )
   }
