77eab80cefebb87938ae4379e514a3617083a478
Alexander <mrbeachcode@gmail.com>
2019-11-10 15:37:43 -0500

    added the find all method to the song repository

--- songs-api/src/main/java/com/example/songsapi/repository/SongRepository.java
+++ songs-api/src/main/java/com/example/songsapi/repository/SongRepository.java
@@ -3,5 +3,9 @@ package com.example.songsapi.repository;
 import com.example.songsapi.model.Song;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SongRepository extends CrudRepository<Song, Long> {
+    List<Song> findAll();
+
 }
