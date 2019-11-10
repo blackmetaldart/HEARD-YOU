96e10db7b55e769cbd38007d47b2106203768654
Alexander <mrbeachcode@gmail.com>
2019-11-10 17:06:07 -0500

    added a blurb to the main page

--- front-end/src/Main.js
+++ front-end/src/Main.js
@@ -6,7 +6,8 @@ class Main extends Component {
       return (
         <div className="Main">
           <h2> Main Page </h2>
-
+          <p>Heard You is a music forum that updates every week with the country's biggest hits!</p>
+          <p>Join our community so that you can chat with industry professionals and fans about the successes of your favorite artists.</p>
         </div>
       );
     }
