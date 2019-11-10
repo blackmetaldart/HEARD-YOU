5a5c9962b40c5ad170f1d0918d3d22440d6a2b9c
Alexander <mrbeachcode@gmail.com>
2019-11-10 12:29:49 -0500

    commented out the request mapping for eureka. issues with mapping

--- users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
+++ users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
@@ -13,8 +13,8 @@ public class UsersApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApiApplication.class, args);
 	}
-	@RequestMapping(value = "/")
-	public String home() {
-		return "Eureka Client : Users application";
-	}
+//	@RequestMapping(value = "/")
+//	public String home() {
+//		return "Eureka Client : Users application";
+//	}
 }
