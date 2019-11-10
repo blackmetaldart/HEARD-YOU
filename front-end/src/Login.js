8231e822fd98edf7482ed7635af954c8bdb17dd9
Alexander <mrbeachcode@gmail.com>
2019-11-10 14:03:18 -0500

    made the template for the login component

---
+++ front-end/src/Login.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+  render(){
+      return (
+        <div className="login">
+          <h3> Log In Here <h3>
+          <p> ENTER USERNAME TEXT BOX HERE <p>
+          <p> ENTER PASSWORD TEXT BOX HERE <p>
+          <p> BUTTON FOR SUBMIT <p>
+        </div>
+      );
+    }
+}
+
+export default Login;
