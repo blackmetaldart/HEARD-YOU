11851c2bfc6afb4c55ceafcccaf5738c5778f27c
Alexander <mrbeachcode@gmail.com>
2019-11-12 15:29:12 -0500

    test. songs template is okay

--- front-end/src/components/Song.js
+++ front-end/src/components/Song.js
@@ -3,14 +3,35 @@ import React, {Component} from 'react';
 class Song extends Component {
   constructor (props){
     super(props);
+
+
   }
-  
+
+makePost = (post) => {
+  fetch("localhost://8083/{username}/makepost", {
+    method : 'POST',
+    headers : {
+      'Authorization' : 'Bearer ' + localStorage.getItem('user'),
+      'Content-Type' : 'application/json'},
+      body : JSON.stringify({
+        description : `${description}`,
+        username : `${username}`,
+        songId : `${songId}`
+      })
+  })
+  .then((response) => ({return response.json()}))
+  .then((response) => ())
+  .catch((error) => ())
+
+  {/*MAKE SUER THAT AFTER REGISTRATION  THE SCREEN SWITCHES TO THE PROFILE COMPONENT*/}
+}
+
   render(){
     return (
       <div>
         <h3>Song : {this.props.songProp.title} [{this.props.songProp.songLength} mins]</h3>
         <h4>{this.props.songProp.artist[0]}</h4>
-        <input type="submit" value="MAKE POST" />
+        <input onClick={() = makePost()} type="submit" value="MAKE POST" />
       </div>
       )
   }
