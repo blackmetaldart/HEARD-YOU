c7691ac939984030f19967204ab19a7de708aad5
Alexander <mrbeachcode@gmail.com>
2019-11-13 13:33:55 -0500

    changed the wording of the home page

--- front-end/src/Main.js
+++ front-end/src/Main.js
@@ -7,7 +7,7 @@ class Main extends Component {
         <div className="Main">
           <h2> Main Page </h2>
           <p>Heard You is a music forum that updates every week with the country's biggest hits!</p>
-          <p>Join our community so that you can chat with industry professionals and fans about the successes of your favorite artists.</p>
+          <p>Join our community so that you can chat with industry professionals and fans alike about the successes of your favorite artists.</p>
         </div>
       );
     }
