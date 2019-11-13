d7234046fb9ecef3451c222f4c5568dad8b167ba
Alexander <mrbeachcode@gmail.com>
2019-11-13 13:29:32 -0500

    removed the commented information in the song component. traded for actual props and state

--- front-end/src/components/Song.js
+++ front-end/src/components/Song.js
@@ -5,12 +5,17 @@ class Song extends Component {
     super(props);
 
     this.makePost = this.makePost.bind(this);
+    this.state = {
+      value : this.props.songProp.songId
+    }
   }
 
 makePost = (e) => {
   e.preventDefault();
-  let song = document.getElementsByClassName('songDiv')
-  const songTitle = song[0].getAttribute("title")
+  let song = document.getElementsByClassName('songDiv');
+  const songTitle = song.getAttribute("title");
+  let description = song.getAttribute("description");
+
   console.log(song);
   console.log(songTitle);
   fetch("localhost://8083/" + this.value + "/makepost", {
@@ -19,26 +24,24 @@ makePost = (e) => {
       'Authorization' : 'Bearer ' + localStorage.getItem('user'),
       'Content-Type' : 'application/json'},
       body : JSON.stringify({
-        description : `${description}`, {/*add data-description*/}
-        username : `${username}`, {/*add data-username*/}
-        songId : `${this.props.songId}`
+        description : `${description}`,
+        username : `${this.props.username}`,
+        songId : `${this.state.songId}`
       })
   })
   .then((response) => {return response.json();})
   .then((response) => {console.log(response);})
   .catch((error) => {console.log(error);})
 
-  {/*MAKE SURE THAT AFTER REGISTRATION  THE SCREEN SWITCHES TO THE PROFILE COMPONENT*/}
-
   {/*set state song added true, if true song has been added*/}
 }
 
   render(){
     return (
-      <div className ="songDiv" >
+      <div className ="songDiv"+ >
         <h3 data-title={this.props.songProp.title}>Song :{this.props.songProp.title} [{this.props.songProp.songLength} mins]</h3>
-        <h4>{this.props.songProp.artist}</h4>
-        <textarea value={this.props.songId} onChange={this.handleChange} cols={80} rows={30} />
+        <h4 data-artist={this.props.songProp.artist}>{this.props.songProp.artist}</h4>
+        <textarea data-songId={this.props.songId} value={this.props.songId} onChange={this.handleChange} cols={80} rows={30} />
         <input onClick={() => makePost()} type="submit" value="MAKE POST" />
       </div>
       )
