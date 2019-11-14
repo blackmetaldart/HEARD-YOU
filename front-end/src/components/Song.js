4202d1463bf4379c2ea6de9a7d3b6fb53429e4cd
Alexander <mrbeachcode@gmail.com>
2019-11-14 10:13:18 -0500

    removed the make post function

--- front-end/src/components/Song.js
+++ front-end/src/components/Song.js
@@ -10,35 +10,13 @@ class Song extends Component {
     }
   }
 
-function makePost(e) {
-  e.preventDefault();
-  let song = document.getElementsByClassName('songDiv');
-  let songTitle = song.getAttribute("title");
-  let description = song.getAttribute("description");
-
-  fetch("localhost://8083/" + this.value + "/makepost", {
-    method : 'POST',
-    headers : {
-      'Authorization' : 'Bearer ' + localStorage.getItem('user'),
-      'Content-Type' : 'application/json'},
-      body : JSON.stringify({
-        description : `${description}`,
-        username : `${this.props.username}`,
-        songId : `${this.state.songId}`
-      })
-  })
-  .then((response) => {return response.json();})
-  .then((response) => {console.log(response);})
-  .catch((error) => {console.log(error);})
-}
-
   render(){
     return (
       <div>
         <h3 data-title={this.props.songProp.title}>Song : {this.props.songProp.title} [{this.props.songProp.songLength} mins]</h3>
         <h4 data-artist={this.props.songProp.artist}>{this.props.songProp.artist}</h4>
         <textarea data-songId={this.props.songId} value={this.props.songId} onChange={this.handleChange} cols={80} rows={30} />
-        <input onClick={() => makePost()} type="submit" value="MAKE POST" />
+        <input type="submit" value="MAKE POST" />
       </div>
       )
   }
