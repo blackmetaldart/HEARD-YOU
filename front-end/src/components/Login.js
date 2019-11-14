3ce3a36e0a22d04d35e42df15abc95ba033bf317
Alexander <mrbeachcode@gmail.com>
2019-11-14 09:48:57 -0500

    rewrote the login function and form to use refs and not data

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -8,38 +8,42 @@ class Login extends Component {
 
   }
 
-  login = (username, password) => {
+  handleSignIn(e) {
+    e.preventDefault()
+    let username = this.refs.username.value;
+    let password = this.refs.password.value;
+    this.props.login(username, password);
+  }
 
+
+  login(username, password) => {
     fetch("localhost://8081/login", {
       method : 'POST',
       headers : {
         'Authorization' : `Bearer ${this.props.jwt}`,
         'Content-Type' : 'application/json'},
       body : JSON.stringify({
-        username : `${username2}`,
-        password : `${password2}`
+        username : `${username}`,
+        password : `${password}`
         })
     })
     .then((response) => {return response.json();})
     .then((response) => {console.log(response);})
     .catch((error) => {console.log(error);})
-
   }
 
   render(){
       return (
         <div className="login">
             <h3> Log In Here </h3>
-            <form onSubmit={this.handleSignIn.bind(this)>
-              <label>
-                USERNAME:
-                <input type="text" ref="username" name="USERNAME" />
+            <form onSubmit={this.handleSignIn.bind(this)}>
+              <label>USERNAME:
+                  <input type="text" ref="username" name="USERNAME" />
               </label>
-              <label>
-                PASSWORD:
-                <input type="text" ref="password" name="PASSWORD" />
+              <label>PASSWORD:
+                  <input type="text" ref="password" name="PASSWORD" />
               </label>
-              <input onSubmit={() => login()} type="submit" value="LOGIN" />
+              <input type="submit" value="LOGIN" />
             </form>
         </div>
       );
@@ -47,25 +51,3 @@ class Login extends Component {
 }
 
 export default Login;
-
-
-// Using a class based component here because we're accessing DOM refs
-
-handleSignIn(e) {
-  e.preventDefault()
-  let username = this.refs.username.value
-  let password = this.refs.password.value
-  this.props.onSignIn(username, password)
-}
-
-
-  return (
-    <form onSubmit={this.handleSignIn.bind(this)}>
-      <h3>Sign in</h3>
-
-
-    </form>
-  )
-}
-
-}
