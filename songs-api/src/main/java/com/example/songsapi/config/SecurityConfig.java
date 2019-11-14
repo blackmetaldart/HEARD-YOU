402da4ebb27fc568710ffa3de938c0311c7569e1
Alexander <mrbeachcode@gmail.com>
2019-11-14 11:56:07 -0500

    added the annotation to the web config

--- songs-api/src/main/java/com/example/songsapi/config/SecurityConfig.java
+++ songs-api/src/main/java/com/example/songsapi/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.songsapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
