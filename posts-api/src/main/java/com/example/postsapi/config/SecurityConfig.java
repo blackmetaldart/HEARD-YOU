afba014a58a9062eb723b9e73ff2a5649c5e1b5c
Alexander <mrbeachcode@gmail.com>
2019-11-14 11:58:44 -0500

    added the annotation to the posts api

--- posts-api/src/main/java/com/example/postsapi/config/SecurityConfig.java
+++ posts-api/src/main/java/com/example/postsapi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.postsapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
