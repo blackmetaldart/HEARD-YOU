e320d2d9d29808b1ab6db6f59f2fb04de2165649
Alexander <mrbeachcode@gmail.com>
2019-11-15 01:08:39 -0500

    added a class name to the post component

--- front-end/src/components/Post.js
+++ front-end/src/components/Post.js
@@ -8,7 +8,7 @@ class Post extends Component {
 
   render(){
     return (
-      <div>
+      <div className="Post">
         <h3>{this.props.postProp.title} [{this.props.postProp.username}]</h3>
         <h4>{this.props.postProp.description}</h4>
       </div>
