c5aceec534d0f06745eb1d1e575d767cad9f96ef
Alexander <mrbeachcode@gmail.com>
2019-11-14 23:23:18 -0500

    removed song length

--- front-end/src/components/Song.js
+++ front-end/src/components/Song.js
@@ -9,7 +9,7 @@ class Song extends Component {
   render(){
     return (
       <div>
-        <h3>Song : {this.props.songProp.title} [{this.props.songProp.songLength} mins]</h3>
+        <h3>Song : {this.props.songProp.title}</h3>
         <h4>{this.props.songProp.artist}</h4>
         <textarea cols={80} rows={10} />
         <button type="submit" value="MAKE POST" />
