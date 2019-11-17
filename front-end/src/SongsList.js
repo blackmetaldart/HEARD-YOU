945e97f886360dbb362d04f6ec53ecd77fbcdc25
Alexander <mrbeachcode@gmail.com>
2019-11-17 11:45:46 -0500

    changed the address for deployment

--- front-end/src/SongsList.js
+++ front-end/src/SongsList.js
@@ -14,7 +14,7 @@ class SongsList extends Component {
 }
 
 componentDidMount() {
-  fetch('http://localhost:8082/songs/list',
+  fetch('http://3.95.190.64:8082/songs/list',
   {
     method : 'GET',
     headers : {
