f675bcd20a351179b715f2320f4eed4489126cf2
Alexander <mrbeachcode@gmail.com>
2019-11-22 16:34:36 -0500

    changed the title in the header component

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render(){
       return (
         <div className="Header">
-          <h1> Listen, Then </h1>
+          <h1> Heard You </h1>
 
         </div>
       );
