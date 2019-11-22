b66fb0559ba776b26ed98a10e761f7e0b766e7b1
Alexander <mrbeachcode@gmail.com>
2019-11-22 14:29:28 -0500

    putting the songs inside of a form. trying to test the make post button

--- front-end/src/components/Song.js
+++ front-end/src/components/Song.js
@@ -9,10 +9,12 @@ class Song extends Component {
   render(){
     return (
       <div className="Song">
+      <form onSubmit={this.makePost.bind(this)}>
         <h3>Song : {this.props.songProp.title}</h3>
         <h4>{this.props.songProp.artist}</h4>
         <textarea cols={80} rows={10} />
         <button type="submit" value="MAKE POST">MAKE POST</button>
+      </form>
       </div>
       )
   }
