42ed9571a6b9681b8f5aedcbfe975d43cc74864a
Alexander <mrbeachcode@gmail.com>
2019-11-22 16:25:46 -0500

    created the handleMakePost method

--- front-end/src/components/Song.js
+++ front-end/src/components/Song.js
@@ -6,13 +6,19 @@ class Song extends Component {
 
   }
 
+handleMakePost(e) {
+  e.preventDefault();
+  let textarea = this.refs.textarea.value;
+  this.makePost(textarea);
+}
+
   render(){
     return (
       <div className="Song">
-      <form onSubmit={this.makePost.bind(this)}>
+      <form onSubmit={this.handleMakePost.bind(this)}>
         <h3>Song : {this.props.songProp.title}</h3>
         <h4>{this.props.songProp.artist}</h4>
-        <textarea cols={80} rows={10} />
+        <textarea ref="textarea"cols={80} rows={10} />
         <button type="submit" value="MAKE POST">MAKE POST</button>
       </form>
       </div>
