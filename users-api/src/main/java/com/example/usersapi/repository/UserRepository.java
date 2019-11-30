0a1bbec9644e255ba3484b03589bae1aaacca8b7
Alexander <mrbeachcode@gmail.com>
2019-11-29 20:26:50 -0500

    commented the user repository for readability

--- users-api/src/main/java/com/example/usersapi/repository/UserRepository.java
+++ users-api/src/main/java/com/example/usersapi/repository/UserRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    // FIND USER BY USERNAME
     User findByUsername (String username);
 
+    // FIND THE USER BY USERNAME AND PASSWORD
     @Query("FROM User u WHERE u.username = ?1 and u.password = ?2")
     User login(String username, String password);
 
