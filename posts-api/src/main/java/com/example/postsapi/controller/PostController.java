18a81dea29685d99cc68d9614768f7bcc8a6357b
Alexander <mrbeachcode@gmail.com>
2019-11-29 20:35:12 -0500

    commented the post controller for readability

--- posts-api/src/main/java/com/example/postsapi/controller/PostController.java
+++ posts-api/src/main/java/com/example/postsapi/controller/PostController.java
@@ -16,18 +16,22 @@ public class PostController {
     @Autowired
     PostService postService;
 
+    // ENDPOINT FOR MAKING A POST WITH A SONG ID
     @PostMapping("{songId}/makepost")
     public Post createPost(@Valid @RequestBody Post post, @Valid @PathVariable Long songId){
         return postService.createPost(post, songId);
     }
 
+    // ENDPOINT FOR GETTING A LIST OF ALL THE POSTS THAT BELONG TO A SONG
     @GetMapping("{songId}/posts")
     public List<Post> getPostsBySongId(@PathVariable Long songId) {return postService.findPostBySongId(songId);}
 
+    // ENDPOINT TO DELETE A POST WITH THE POSTID
     @DeleteMapping ("/post/{postId}")
     public ResponseEntity<Object> deletePostByPostId(@PathVariable Long postId) { return postService.deletePostByPostId(postId);
     }
 
+    // ENDPOING THAT RETURNS A LIST OF THE POSTS
     @GetMapping("/list")
     public Iterable<Post> listPosts(){
         return postService.findAllPosts();
