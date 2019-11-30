a0637c74458002fcb9b6fa1f65d3206c7a8e0c8c
Alexander <mrbeachcode@gmail.com>
2019-11-29 20:40:53 -0500

    commented the post service for readability

--- posts-api/src/main/java/com/example/postsapi/service/PostServiceImpl.java
+++ posts-api/src/main/java/com/example/postsapi/service/PostServiceImpl.java
@@ -16,27 +16,32 @@ public class PostServiceImpl implements PostService {
     @Autowired
     PostRepository postRepository;
 
+    // CREATE A POST WITH A POST BODY AND A SONG ID
     @Override
     public Post createPost(Post post, Long songId) {
         post.setSongId(songId);
         return postRepository.save(post);
     }
 
+    // GET ALL THE POSTS WITH A USERNAME
     @Override
     public List<Post> getPostsByUsername(String username) {
         return postRepository.findAllByUsername(username);
     }
 
+    // GET A POST WITH A POSTID
     @Override
     public Post findPostByPostId(Long postId) {
         return postRepository.getPostById(postId);
     }
 
+    // GET A LIST OF POSTS WITH A SONGID
     @Override
     public List<Post> findPostBySongId(Long songId) {
         return postRepository.getPostsBySongId(songId);
     }
 
+    // DELETE POST WITH A POSTID
     @Override
     public ResponseEntity<Object> deletePostByPostId(Long postId){
         return postRepository.findById(postId).map (post -> {
@@ -45,6 +50,7 @@ public class PostServiceImpl implements PostService {
         }).orElseThrow(() -> new ExceptionHandler("PostId " + postId + " not found"));
     }
 
+    // GET A LIST OF ALL THE POSTS IN THE REPOSITORY
     @Override
     public Iterable<Post> findAllPosts(){return postRepository.findAll();}
 }
