cdb22dcd5dd601bce906506ae15fe79bc94e31a6
Alexander <mrbeachcode@gmail.com>
2019-11-29 20:37:36 -0500

    commented the post repository for readability

--- posts-api/src/main/java/com/example/postsapi/repository/PostRepository.java
+++ posts-api/src/main/java/com/example/postsapi/repository/PostRepository.java
@@ -7,15 +7,20 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
+
+    // GET A LIST OF POSTS WITH THE USERNAME
     @Query("FROM Post post WHERE post.username = ?1")
     List<Post> findAllByUsername(String username);
 
+    // GET A POST WITH THE POSTID
     @Query("FROM Post post WHERE post.id = ?1")
     Post getPostById (Long postId);
 
+    // GET A LIST OF POSTS WITH A SONGID
     @Query("FROM Post post WHERE post.songId =?1")
     List<Post> getPostsBySongId (Long songId);
 
+    // GET A LIST OF ALL THE POSTS IN THE REPOSITORY
     List<Post> findAll();
 
 }
