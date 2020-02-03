85f09cbd5ddb43c294129bb71542fffbf201b814
Alexander <mrbeachcode@gmail.com>
2020-02-03 16:51:51 -0500

    turned the footer into a functional component and put all the css in the js file

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,15 +1,17 @@
-import React, {Component} from 'react';
-import './Footer.css';
+import React from 'react';
 
-class Footer extends Component {
-  render(){
+function Footer() {
+  const footerStyle = {
+      position : 'absolute',
+      bottom : '0',
+      width : '100vw',
+      textAlign : 'center'
+  }
       return (
-        <div className="Footer">
+        <div style={footerStyle} className="Footer">
           <h5> &copy; Alexander Beach </h5>
-
         </div>
       );
-    }
 }
 
 export default Footer;
